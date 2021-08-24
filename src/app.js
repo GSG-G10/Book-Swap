4150502b69b95fbc7e01590e3983e79dafb1a0ca
Noorin99 <noorin.sa.99@gmail.com>
2021-08-24 19:07:05 +0300

    creating a server Relates #6

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { join } = require('path');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, '..', 'public')));
+
+app.use(router);
