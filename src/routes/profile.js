0c03f1517f60b5e851fd356156212ca8e04508c8
Noorin99 <noorin.sa.99@gmail.com>
2021-08-25 11:44:56 +0300

    Changing some http methods Relates #17

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -5,7 +5,7 @@ router.get('/profile', generateError);
 router.get('/profile/:username', getUser);
 router.post('/profile/:username/add', addBook);
 router.put('/profile/:username/edit', editBook);
-router .get('/profile/:username/delete', deleteBook);
-router.get('/profile/:username/return', returnBook);
+router .delete('/profile/:username/delete', deleteBook);
+router.delete('/profile/:username/return', returnBook);
 
 module.exports = router;
