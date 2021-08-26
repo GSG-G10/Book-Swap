16fa17ef585cecbf4f78bcc8b5c2df7b04f1e513
reemsh2000 <sh951652@gmail.com>
2021-08-26 09:53:03 +0300

    edit port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const app = require('./app');
-
-app.listen(3000 , () => {
+const port = process.env.PORT || 3000;
+app.listen(port , () => {
     console.log('Server is working! Yahooooooooo!');
 } );
-
